be2afa11e4133da5740ec43f93bcb1532ed3c125
mk2 <mk2@mk2.red>
2018-06-06 11:02:19 +0900

    add sync_master.sh

---
+++ sync_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+master_added_files=`git diff origin/master --diff-filter=D --name-status | awk '{ print $2 }'`
+master_deleted_files=`git diff origin/master --diff-filter=A --name-status | awk '$2 !~ /^_ja.*/ { print $2 }'`
+
+# remove files
+for file in ${master_deleted_files}; do
+  echo "REMOVE: ${file}"
+done
+
+# create files
+for file in ${master_added_files}; do
+  echo "CREATE: ${file}"
+done
